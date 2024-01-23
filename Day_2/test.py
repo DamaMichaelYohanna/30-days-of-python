cba328fc7f3ee354995b86e91a695a22428ea5bb
= <get2dama11@gmail.com>
2024-01-23 22:00:24 +0100

    Test for functionality

---
+++ Day_2/test.py
@@ -0,0 +1,31 @@
+import unittest
+from main import DoubleLinkedList
+
+singlylinkedlist: DoubleLinkedList = DoubleLinkedList()
+
+
+class MyTestCase(unittest.TestCase):
+
+    def test_append(self):
+        """test if item is added successfully."""
+        self.assertEqual(singlylinkedlist.append("hello"), None)
+
+    def test_length(self):
+        """test the length of the singlylinkedlist"""
+        self.assertEqual(len(singlylinkedlist), 1)  # add assertion here
+
+    def test_remove(self):
+        """test if item is remove successfully."""
+        singlylinkedlist.remove("hello")
+        self.assertEqual(len(singlylinkedlist), 0)
+
+    def test_search(self):
+        found = singlylinkedlist.search("hello")
+        self.assertEqual(found, False)
+
+    def test_clear(self):
+        singlylinkedlist.clear()
+        self.assertEquals(singlylinkedlist.head, None)
+
+if __name__ == '__main__':
+    unittest.main()
